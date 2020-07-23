083d1c9369f036edbd3b58d8d1bcbe1ac046a287
kaperskyguru <solomoneseme@gmail.com>
2020-07-23 14:46:05 +0100

    Bug fixes on post

--- components/Post.vue
+++ components/Post.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="card">
-    <img class="card-img-top" :src="image" />
+    <div class="b-block">
+      <nuxt-link to="#">
+        <img class="card-img-top" :src="image" :alt="post.title" />
+      </nuxt-link>
+    </div>
     <div class="card-body">
       <p class="subtitle is-6">{{ post.author.name }}</p>
 
@@ -37,4 +41,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.card-img-top {
+  width: 100%;
+  height: 100%;
+}
+</style>
