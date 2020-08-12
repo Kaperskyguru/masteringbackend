468aca53fd966a45821f3adca2f20d82734594db
kaperskyguru <solomoneseme@gmail.com>
2020-08-12 11:36:10 +0100

    Worked on SEO and Slack Invite

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,17 +22,116 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Mastering Backend Development',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      { hid: 'name', name: 'title', content: 'Mastering Backend Development' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content:
+          'The ultimate backend development blog for backend developers and engineers.',
       },
+
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'backend,web development,backend development,laravel,backend developer,software development',
+      },
+      { hid: 'robots', name: 'robots', content: 'index, follow' },
+      {
+        hid: 'http-equiv',
+        'http-equiv': 'Content-Type',
+        content: 'text/html; charset=utf-8',
+      },
+      { hid: 'language', name: 'language', content: 'English' },
+      { hid: 'revisit-after', name: 'revisit-after', content: '7 days' },
+      { hid: 'author', name: 'author', content: 'Solomon Eseme' },
+
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      {
+        name: 'msapplication-TileImage',
+        content: '/favicon/ms-icon-144x144.png',
+      },
+      { name: 'theme-color', content: '#ffffff' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+
+    link: [
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '57x57',
+        href: '/favicon/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/favicon/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '72x72',
+        href: '/favicon/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/favicon/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '114x114',
+        href: '/favicon/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/favicon/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '144x144',
+        href: '/favicon/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/favicon/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-icon-180x180.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/favicon/android-icon-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/favicon/manifest.json' },
+    ],
 
     script: [{ src: 'https://use.fontawesome.com/3889c7d65e.js' }],
   },
