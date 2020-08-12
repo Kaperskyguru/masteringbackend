a86da86db1520faaa9a1e9940202188587b3db6c
kaperskyguru <solomoneseme@gmail.com>
2020-08-12 15:49:40 +0100

    Fixing Maintenance mode bugs

--- middleware/maintenance.js
+++ middleware/maintenance.js
@@ -1,16 +1,15 @@
 export default function ({ redirect, route, store }) {
-  const isDev = process.env.NODE_ENV === 'production'
+  const isProd = process.env.NODE_ENV === 'production'
   const isMaintenance = process.env.MAINTENANCE_MODE || false
 
   const { isCounterEnded } = store.state
 
-  if (isMaintenance === true || isDev === true) {
-    console.log(isMaintenance, isDev, isCounterEnded)
-    if (isCounterEnded === false) return redirect('/maintenance')
+  if (isMaintenance === true && isProd === true && isCounterEnded === false) {
+    return redirect('/maintenance')
   }
 
   if (
-    (isCounterEnded === true || isMaintenance === false || isDev === false) &&
+    (isCounterEnded === true || isMaintenance === false || isProd === false) &&
     route.path === '/maintenance'
   ) {
     return redirect('/')
