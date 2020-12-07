c1430fb22d65befdff358d4dbfe32c2d6b5f4acd
Solomon Eseme <kaperskyguru@gmail.com>
2020-12-07 14:40:02 +0100

    Fix BASE URL

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,13 +12,18 @@ export default {
   // beforeRouteEnter(to, from, next) {
   //   console.log(from, to)
   // },
+  data() {
+    return {
+      baseURL: process.env.BASE_URL || 'https://masteringbackend.com',
+    }
+  },
   head() {
     console.log(`${process.env.BASE_URL}${this.$route.path}`)
     return {
       link: [
         {
           rel: 'canonical',
-          href: `${process.env.BASE_URL}${this.$route.path}`,
+          href: `${this.baseURL}${this.$route.path}`,
         },
       ],
     }
