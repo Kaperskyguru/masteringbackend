d38699e47781030f229c1cb58ab2e4be1d5fa95c
Solomon Eseme <kaperskyguru@gmail.com>
2020-12-16 10:50:49 +0100

    Bug Fix on Image URL

--- components/SinglePost.vue
+++ components/SinglePost.vue
@@ -92,7 +92,11 @@ export default {
     image() {
       if (this.post) {
         if (this.post.thumbnail_images) {
-          return this.post.thumbnail_images['post-thumbnail'].url
+          if (this.post.thumbnail_images['post-thumbnail']) {
+            return this.post.thumbnail_images['post-thumbnail'].url
+          } else {
+            return this.post.thumbnail_images.full.url
+          }
         }
       }
       return '/img/default_banner.webp'
