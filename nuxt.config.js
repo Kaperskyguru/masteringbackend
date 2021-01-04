a0df0edcba83f71b6a4c4693a9226c6f3d935579
Solomon Eseme <kaperskyguru@gmail.com>
2021-01-04 08:15:27 +0100

    URL redirect

--- nuxt.config.js
+++ nuxt.config.js
@@ -310,7 +310,7 @@ export default {
       statusCode: 301,
     },
     {
-      from: '^posts/laravel-framework-the-ultimate-guide-2021$',
+      from: '^posts/laravel-framework-the-ultimate-guide-2021(.*)$',
       to: 'posts/laravel-framework-the-ultimate-guide$1',
       statusCode: 301,
     },
