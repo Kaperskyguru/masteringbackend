64e050fa6c5598b5b2ba6b454f75f2567c349c03
Solomon Eseme <kaperskyguru@gmail.com>
2021-01-24 00:52:49 +0100

    Still testing ad display

---
+++ components/InlineAdvert.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="car mb-3">
+    <div>
+      <adsbygoogle
+        :adFullWidthResponsive="adFullWidthResponsive"
+        :ad-slot="adslot"
+        :ad-format="adformat"
+        :ad-style="mystyle"
+        :ad-layout-key="addLayoutKey"
+        :ad-layout="adLayout"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'adFullWidthResponsive',
+    'adslot',
+    'adformat',
+    'mystyle',
+    'adLayoutKey',
+  ],
+}
+</script>
+
+<style >
+</style>
